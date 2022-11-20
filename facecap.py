2b2fff35a55885ddf476bf17c5240d53b9bb547d
dilip2048 <dilipptt@gmail.com>
2022-11-20 14:14:54 -0600

    Capture Human Emotion

--- facecap.py
+++ facecap.py
@@ -1,8 +1,17 @@
 import cv2
 import mediapipe as mp
+from keras.models import load_model
+from keras_preprocessing.image import img_to_array
+import numpy as np
+
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+face_classifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('/Users/dilipyadav/githome/erebor/model/EmotionDetectionModel.h5')
+
+class_labels=['Angry','Happy','Neutral','Sad','Surprise']
 
 webcam = cv2.VideoCapture(0)
 ##########################################################################################
@@ -56,17 +65,42 @@ while webcam.isOpened():
                 f.write('----------')
             f.close()
 
+        # Emotion detection code starts from here
+        labels = []
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+
+                preds = classifier.predict(roi)[0]
+                label = class_labels[preds.argmax()]
+                label_position = (x, y)
+                cv2.putText(img, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            else:
+                cv2.putText(img, 'No Face Found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            # emotion detection code ends here
+
 ##########################################################################################
     # text: output inage, text, position, font, scale, color, thickness, parameter
     img = cv2.putText(img, "Press the 'Q' key to exit the program", (20, 20),
                       cv2.FONT_HERSHEY_DUPLEX, 0.5, (225, 225, 225), 1, cv2.LINE_AA)
     img = cv2.putText(img, "(The X window button won't work...)", (20, 40),
                       cv2.FONT_HERSHEY_DUPLEX, 0.5, (225, 225, 225), 1, cv2.LINE_AA)
-    img = cv2.putText(img, "Current Expression: ", (20, 440),
-                      cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 0), 2, cv2.LINE_AA)
-    img = cv2.putText(img, "Current Expression: ", (20, 440),
-                      cv2.FONT_HERSHEY_DUPLEX, 0.7, (225, 225, 225), 1, cv2.LINE_AA)
-    
+    # img = cv2.putText(img, "Current Expression: ", (20, 440),
+    #                   cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 0), 2, cv2.LINE_AA)
+    # img = cv2.putText(img, "Current Expression: ", (20, 440),
+    #                   cv2.FONT_HERSHEY_DUPLEX, 0.7, (225, 225, 225), 1, cv2.LINE_AA)
+
+
+
     # output video window
     cv2.imshow('Erebor 3D Emotion Recognition System', img)
     if cv2.waitKey(5) & 0xFF == ord('q'):

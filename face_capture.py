bb9dfed7405981da49926d2a2348b305ab6297f5
dilip2048 <dilipptt@gmail.com>
2022-12-03 19:39:52 -0600

    Made chances to make it work on lemuels laptop

--- face_capture.py
+++ face_capture.py
@@ -44,6 +44,8 @@ while webcam.isOpened():
 
     # Emotion detection code starts from here
     labels = []
+
+    label = ""
     """Changed the color of the image to grey, because we have trained model on grey images"""
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -122,10 +124,10 @@ while webcam.isOpened():
                 connection_drawing_spec=val_c
             )
 
-            with open('framedata.txt', 'w') as f:
-                f.write(str(face_landmarks))
-                f.write('----------')
-            f.close()
+            # with open('framedata.txt', 'w') as f:
+            #     f.write(str(face_landmarks))
+            #     f.write('----------')
+            # f.close()
 
     ##########################################################################################
     # text: output inage, text, position, font, scale, color, thickness, parameter
@@ -133,6 +135,13 @@ while webcam.isOpened():
                       cv2.FONT_HERSHEY_DUPLEX, 0.5, (225, 225, 225), 1, cv2.LINE_AA)
     img = cv2.putText(img, "(The X window button won't work...)", (20, 40),
                       cv2.FONT_HERSHEY_DUPLEX, 0.5, (225, 225, 225), 1, cv2.LINE_AA)
+
+    cv2.rectangle(img, (10, 670), (300, 590), (232, 52, 235), 2)
+
+    label_position = (20, 650)
+    """Display the predicted emotion on the side of the face"""
+    cv2.putText(img, label, label_position, cv2.FONT_HERSHEY_TRIPLEX, 2, (52, 217, 235), 3)
+
     # img = cv2.putText(img, "Current Expression: ", (20, 440),
     #                   cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 0), 2, cv2.LINE_AA)
     # img = cv2.putText(img, "Current Expression: ", (20, 440),
